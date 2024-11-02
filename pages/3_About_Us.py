03de2e2c7aaeeb647cd577ada499e4de931fd5cd
bencmbit98 <104804447+bencmbit98@users.noreply.github.com>
2024-11-02 09:01:23 +0800

    Update 3_About_Us.py
    Amended Disclaimer!

--- pages/3_About_Us.py
+++ pages/3_About_Us.py
@@ -16,7 +16,7 @@ with st.expander("How to use this App"):
     st.write("2. Click the 'Submit' button.")
     st.write("3. The app will generate a text completion based on your prompt.")
 
-with st.expander("How to use this App"):
+with st.expander("Important Disclaimer"):
     st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters.")
     st.write("Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.")
     st.write("Always consult with qualified professionals for accurate and personalized advice.")
